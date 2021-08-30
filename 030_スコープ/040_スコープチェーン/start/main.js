7f8360b1e802c0626223324358028c7e563b4e94
Taka2401 <tkht2401@gmail.com>
2021-08-30 21:12:25 +0900

    scope(block)

--- 030_スコープ/040_スコープチェーン/start/main.js
+++ 030_スコープ/040_スコープチェーン/start/main.js
@@ -3,16 +3,17 @@ window.a = 4;
 function fn1() {
     // let a = 1;
     function fn2() {
+        let a = 2; //順番にfn2⇒fn1⇒aにaを探しに行く
         console.log(a);
-
         if (true) {
             var a = 3;
         }
-
     }
     fn2();
 }
 fn1();
+
+
 
 
 
