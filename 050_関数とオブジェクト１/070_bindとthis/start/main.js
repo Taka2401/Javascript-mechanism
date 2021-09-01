9ee51fb5c1563177322eb456e4854e619cb4598f
Taka2401 <tkht2401@gmail.com>
2021-09-01 20:57:58 +0900

    bindとthis

--- 050_関数とオブジェクト１/070_bindとthis/start/main.js
+++ 050_関数とオブジェクト１/070_bindとthis/start/main.js
@@ -8,8 +8,23 @@ const person = {
 }
 person.hello();
 
+// bindで参照先のオブジェクトを指定
+const helloTom = person.hello.bind(person);
+
 function fn(ref) {
     ref();
 }
 
-fn(person.hello);
+fn(helloTom);
+
+function a(name) {
+    console.log('hello ' + name);
+}
+
+// const b = a.bind({name: 'Tim'});
+
+// bindで引数を固定
+const b = a.bind(null, 'Tim');
+
+// bindが優先される
+b('Tom');
