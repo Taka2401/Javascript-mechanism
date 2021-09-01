0775bdeae29fbdbd7c9931ece3b4d5d1b9f917f2
Taka2401 <tkht2401@gmail.com>
2021-09-01 14:35:43 +0900

    参照と値の比較

--- 040_変数/100_参照の比較と値の比較/end/main.js
+++ 040_変数/100_参照の比較と値の比較/end/main.js
@@ -6,9 +6,13 @@ const b = {
     prop: 0
 }
 
+// 参照先のオブジェクトが変わるため、falseになる
 console.log(a == b);
-console.log(a.prop === b.prop);
+
+// オブジェクトのプロパティを比較する
+console.log(a.prop === b.prop); //true
 
 const c = a;
 
+// 参照先が一致し、trueになる
 console.log(a === c);
