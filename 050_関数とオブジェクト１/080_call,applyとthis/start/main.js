cf688429de8adbee275bd46758c56fcda833a700
Taka2401 <tkht2401@gmail.com>
2021-09-01 21:17:58 +0900

    call apply

--- 050_関数とオブジェクト１/080_call,applyとthis/start/main.js
+++ 050_関数とオブジェクト１/080_call,applyとthis/start/main.js
@@ -1,7 +1,24 @@
-function a(name) {
-    console.log('hello ' + name);
+function a(name, name1) {
+    console.log('hello ' + name + ' ' + name1);
 }
 
-const b = a.bind(null, 'Tim');
+const tim = {name: 'Tim'};
+
+const b = a.bind(tim);
 
 b();
+
+// 実行も同時にされる
+
+// apply 第一引数に参照先のオブジェクト 第二に値
+a.apply(tim, ['Tim', 'Bob']);
+
+// call 第一引数に参照先のオブジェクト　第二に引数
+a.call(tim, 'Tim', 'Bob');
+
+const array = [1,2,3,4,5];
+
+// applyを使い配列の値を一つずつ渡す
+// const result = Math.max.apply(null, array);
+const result = Math.max(...array); //ES6
+console.log(result);
