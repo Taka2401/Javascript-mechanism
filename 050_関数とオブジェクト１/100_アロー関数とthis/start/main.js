c41c8ef49d9a8c76790d3495ffe877e7c3049178
Taka2401 <tkht2401@gmail.com>
2021-09-01 21:40:28 +0900

    アロー関数とthis

--- 050_関数とオブジェクト１/100_アロー関数とthis/start/main.js
+++ 050_関数とオブジェクト１/100_アロー関数とthis/start/main.js
@@ -1,7 +1,9 @@
+// アロー関数ではレキカルスコープを参照
 window.name = 'John';
 
 const person = {
     name: 'Tom',
+    // アロー関数だとthisやargumentを使えない
     hello: function() {
         console.log('Hello ' + this.name);
     }
