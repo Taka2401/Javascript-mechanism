1b676321a9dbec1e90b456d258721422c6960432
Taka2401 <tkht2401@gmail.com>
2021-09-03 21:29:12 +0900

    プロトタイプ継承

--- 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
+++ 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
@@ -6,3 +6,28 @@ function Person(name, age) {
 Person.prototype.hello = function() {
   console.log('hello ' + this.name);
 }
+
+// コンストラクタの継承
+function Japanese(name, age, gender) {
+  Person.call(this, name, age);
+  this.gender = gender;
+}
+
+// Person.prototypeを継承できる
+Japanese.prototype = Object.create(Person.prototype);
+
+// helloを上書き
+Japanese.prototype.hello = function() {
+  console.log('Konnichiwa ' + this.name);
+}
+
+// メソッドの追加
+Japanese.prototype.bye = function() {
+  console.log('Sayonara ' + this.name);
+}
+
+const taro = new Japanese('Taro', 23, 'Male');
+console.log(taro);
+
+taro.hello();
+taro.bye();
