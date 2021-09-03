99b27009b4a4e83034db9ddbd36b9d58ea61a98a
Taka2401 <tkht2401@gmail.com>
2021-09-03 20:21:32 +0900

    プロトタイプ

--- 060_関数とオブジェクト２/020_prototype/start/main.js
+++ 060_関数とオブジェクト２/020_prototype/start/main.js
@@ -1,6 +1,10 @@
 function Person(name, age) {
     this.name = name;
     this.age = age;
+    // これでもできる
+    // this.hello = function() {
+    //     console.log('hello ' + this.name);
+    // }
 }
 
 // メソッドとして追加したいプロパティをprototypeオブジェクトに格納できる
@@ -9,10 +13,12 @@ Person.prototype.hello = function() {
     console.log('hello ' + this.name);
 }
 
-
 const bob = new Person('Bob', 18);
 const tom = new Person('Tom', 33);
 const sun = new Person('Sun', 20);
 
 bob.hello();
 tom.hello();
+
+// prototypeを使う理由
+// インスタンスを生成する度にhelloが追加されメモリが余分に消費する
