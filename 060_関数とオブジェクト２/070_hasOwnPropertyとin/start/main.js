54094822d8089b1ba9d76337a9663e93116a6b86
Taka2401 <tkht2401@gmail.com>
2021-09-03 21:14:10 +0900

    hasOwnPropertyとin

--- 060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
+++ 060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
@@ -1,10 +1,17 @@
 function Person(name, age) {
-    this.name = name;
-    this.age = age;
+	this.name = name;
+	this.age = age;
 }
 
 Object.prototype.hello = function() {
-    console.log('Object: hello ' + this.name);
+	console.log('Object: hello ' + this.name);
 }
 
 const bob = new Person('Bob', 18);
+
+// 自分自身のプロパティとして存在するか確認
+const result = bob.hasOwnProperty('name')
+console.log(result)
+
+// プロトタイプチェーンまで含めて確認
+console.log('hasOwnProperty' in bob);
