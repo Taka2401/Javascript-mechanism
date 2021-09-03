4db61c5da4c65c88e1e378b3e0e26c0bd5a86de8
Taka2401 <tkht2401@gmail.com>
2021-09-03 21:11:14 +0900

    プロトタイプチェーン

--- 060_関数とオブジェクト２/060_プロトタイプチェーン/start/main.js
+++ 060_関数とオブジェクト２/060_プロトタイプチェーン/start/main.js
@@ -1,11 +1,25 @@
 function Person(name, age) {
   this.name = name;
   this.age = age;
+  // 呼び出される優先順位
+  // 1 自分のプロパティ
+  // this.hello = function() {
+  //     console.log('OwnProperty: hello ' + this.name);
+  // }
 }
 
-Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
+// 2 Person.prototype
+// Person.prototype.hello = function() {
+//     console.log('Person: hello ' + this.name);
+// }
+
+// 3 Objectのプロパティ
+Object.prototype.hello = function() {
+  console.log('Object: hello ' + this.name);
 }
 
 const bob = new Person('Bob', 18);
 bob.hello();
+
+const result = bob.hasOwnProperty('name');
+console.log(result);
