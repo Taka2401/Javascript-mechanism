1e1259207afbc1b761b2935d94a96d4167569af4
Taka2401 <tkht2401@gmail.com>
2021-09-03 19:13:03 +0900

    コンストラクター関数

---
+++ 060_関数とオブジェクト２/010_コンストラクター関数/start/main.js
@@ -0,0 +1,16 @@
+// コンストラクター関数
+// 新しくオブジェクトを作成するための雛形となる関数
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+const bob = new Person('Bob', 18);
+const tom = new Person('Tom', 33);
+const sun = new Person('Sun', 20);
+
+// インスタンス・・newで作成したオブジェクト
+// bob, tom, sun
+
+// newでオブジェクトを作成することをインスタンス化という
